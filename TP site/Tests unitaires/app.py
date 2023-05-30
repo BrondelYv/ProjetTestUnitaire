c58574d5195a37ec47500a8ed97d9b9adbbf2b41
BerkKayin <63935523+BerkKayin@users.noreply.github.com>
2023-05-30 17:41:09 +0200

    Add files via upload

---
+++ TP site/Tests unitaires/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- 
+from flask import Flask, render_template  
+import pandas as pd
+
+app = Flask(__name__)  
+df = pd.read_csv('drought-tv-news.csv')
+df.to_csv('drought-tv-news.csv', index=None)
+
+@app.route("/") 
+@app.route('/table')
+
+def table():
+    data = pd.read_csv('drought-tv-news.csv')
+    return render_template('table.html', tables=[data.to_html()], titles=[''])
+
+if __name__ == "__main__":     
+    app.run(debug=True)
